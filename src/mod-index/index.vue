9f01ffc4e03aa0849549a5c15962b56e2015966e
cloudtian <624714349@qq.com>
2018-03-28 00:37:56 +0800

    [init]初始化vuex-test项目

---
+++ src/mod-index/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <counter></counter>
+    </div>
+</template>
+
+<script>
+import Counter from '../store/count/count.vue';
+import store from '../store/count/count.js';
+
+export default {
+  store,
+  components: {
+    Counter
+  }
+};
+</script>
---
+++ src/store/count/count.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>计数器：{{ count }}</div>
+        <button @click="increment">增加</button>
+        <button @click="decrement">减少</button>
+    </div>
+    
+</template>
+<script>
+export default {
+  name: 'Counter',
+  computed: {
+    count () {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment');
+    },
+    decrement () {
+      this.$store.commit('decrement');
+    }
+  }
+};
+</script>
