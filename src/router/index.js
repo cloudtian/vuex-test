182be4918e02dfc63814d3f0d76530ee17ff6888
cloudtian <cloud_tian@foxmail.com>
2018-04-09 20:29:23 +0800

    [update]添加$nextTick测试

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Index from '../mod-index/index';
+import Test from '../mod-test/test';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 });
